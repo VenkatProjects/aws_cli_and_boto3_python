1f84c441d0db9aa231db1d6de951e751fbd14bf4
Venkat <67817741+VenkatProjects@users.noreply.github.com>
2023-06-29 20:52:29 +0530

    Update ec2_delete.py

--- ec2/ec2_delete.py
+++ ec2/ec2_delete.py
@@ -4,12 +4,10 @@ import sys
 # Create an EC2 resource
 ec2_resource = boto3.resource('ec2')
 
-# Retrieve the instance ID from command-line argument
 if len(sys.argv) < 2:
-    print("Please provide the instance ID as a command-line argument.")
+    print("Please provide the instance ID: ")
     sys.exit(1)
 instance_id = sys.argv[1]
-# Retrieve the instance object
 instance = ec2_resource.Instance(instance_id)
 # Terminate the instance
 try:
@@ -18,4 +16,3 @@ try:
     print("Termination successful!")
 except Exception as e:
     print("Error occurred while terminating the instance:", str(e))
-
