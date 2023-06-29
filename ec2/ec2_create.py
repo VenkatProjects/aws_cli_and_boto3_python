f1531b04f04c1a283884c63a007366d4fd436030
Venkat <67817741+VenkatProjects@users.noreply.github.com>
2023-06-29 20:53:30 +0530

    Update ec2_create.py

--- ec2/ec2_create.py
+++ ec2/ec2_create.py
@@ -7,8 +7,8 @@ ec2_resource = boto3.resource('ec2')
 instance = ec2_resource.create_instances(
         ImageId='ami-0d2f97c8735a48a15',
         InstanceType='t2.micro',
-        MinCount=1,
-        MaxCount=1,
+        MinCount=1, #Enter number instance you want
+        MaxCount=1, 
         KeyName='test2devops',
         SecurityGroupIds=['sg-079b0d65af0d1fd'],
         SubnetId='subnet-0ed9764e7d7254629b7b'
@@ -16,4 +16,3 @@ instance = ec2_resource.create_instances(
 # Access the created instance's ID
 instance_id = instance[0].instance_id
 print(f"Created EC2 Instance ID: {instance_id}")
-
